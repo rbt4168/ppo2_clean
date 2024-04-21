6f4071cd0102ccc3a6129eb4f3a368ad16739630
rbt4168 <rbt4168@gmail.com>
2024-04-21 18:56:33 +0800

    remove unused

--- helper.py
+++ helper.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 import torch
 
@@ -11,24 +10,6 @@ def ensure_dir(file_path):
     if not os.path.exists(directory):
         os.makedirs(directory)
     return file_path
-
-def safemean(xs):
-    return np.nan if len(xs) == 0 else np.mean(xs)
-
-def explained_variance(ypred, y):
-    """
-    Computes fraction of variance that ypred explains about y.
-    Returns 1 - Var[y-ypred] / Var[y]
-
-    interpretation:
-        ev=0  =>  might as well have predicted zero
-        ev=1  =>  perfect prediction
-        ev<0  =>  worse than just predicting zero
-
-    """
-    assert y.ndim == 1 and ypred.ndim == 1
-    vary = np.var(y)
-    return np.nan if vary==0 else 1 - np.var(y-ypred)/vary
 
 def save_checkpoint(state, path, filename='checkpoint.pth.tar', version=0):
     torch.save(state, ensure_dir(os.path.join(path, version, filename)))
